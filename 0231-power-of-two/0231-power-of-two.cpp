b1cee4ed06478cc54bfb03a732a98ba4967646f6
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2023-04-01 12:30:36 +0600

    Time: 3 ms (52.43%), Space: 5.9 MB (83.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        if(n<0) return false;
+        if(n<=0)return false;
+        if(n==1)return true;
+        if(n&1)return false;
         
-        for(int i=0; i<= n; i++)
-        {
-            long long x = 1LL*pow(2,i);
-            if(x == 1LL*n)return true;
-            if(x> 1LL*n)return false;
-        }
-        
-        return true;
+        return isPowerOfTwo((n>>1));
     }
 };
